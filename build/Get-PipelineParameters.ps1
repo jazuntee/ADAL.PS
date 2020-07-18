b009d10b3f0d677bffd3117b9bf7c7bdc1ec9a43
Jason Thompson <jasoth@microsoft.com>
2020-07-18 14:06:52 -0400

    update

--- build/Get-PipelineParameters.ps1
+++ build/Get-PipelineParameters.ps1
@@ -18,10 +18,9 @@ Import-Module "$PSScriptRoot\CommonFunctions.psm1" -Force -WarningAction Silentl
 $ModuleManifest = Import-PowerShellDataFile $ModuleManifestFileInfo.FullName -ErrorAction Stop
 
 Write-Host ('##vso[task.setvariable variable=moduleName;isOutput=true]{0}' -f $ModuleManifestFileInfo.BaseName)
-Write-Host ('##[debug] {0} = {1}' -f 'moduleName', $env:moduleName)
+Write-Host ('##[debug] {0} = {1}' -f 'moduleName', $ModuleManifestFileInfo.BaseName)
 
-Write-Host ('##vso[task.setvariable variable=moduleVersion;]{0}' -f $ModuleManifest.ModuleVersion)
-Write-Host ('##[debug] {0} = {1}' -f 'moduleVersion', $env:MODULEVERSION)
+Write-Host ('##vso[task.setvariable variable=moduleVersion;isOutput=true]{0}' -f $ModuleManifest.ModuleVersion)
 Write-Host ('##[debug] {0} = {1}' -f 'moduleVersion', $ModuleManifest.ModuleVersion)
 
 ## Read Packages Configuration
@@ -29,6 +28,6 @@ $xmlPackagesConfig = New-Object xml
 $xmlPackagesConfig.Load($PackagesConfigFileInfo.FullName)
 
 foreach ($package in $xmlPackagesConfig.packages.package) {
-    Write-Host ('##vso[task.setvariable variable=version.{0};]{1}' -f $package.id, $package.version)
+    Write-Host ('##vso[task.setvariable variable=version.{0};isOutput=true]{1}' -f $package.id, $package.version)
     Write-Host ('##[debug] version.{0} = {1}' -f $package.id, $package.version)
 }
