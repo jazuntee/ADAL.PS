1977b1baa85ecae7a6a74ed1579d8d58ef41f319
Jason Thompson <jasoth@microsoft.com>
2020-07-18 22:09:31 -0400

    update

--- build/Get-PSModuleInfo.ps1
+++ build/Get-PSModuleInfo.ps1
@@ -5,7 +5,10 @@ param
     [string] $ModuleManifestPath = "..\src",
     #
     [parameter(Mandatory = $false)]
-    [string] $PackagesConfigPath = "..\"
+    [string] $PackagesConfigPath = "..\",
+    #
+    [parameter(Mandatory = $false)]
+    [switch] $TrimRevisionNumber
 )
 
 ## Initialize
@@ -17,11 +20,15 @@ Import-Module "$PSScriptRoot\CommonFunctions.psm1" -Force -WarningAction Silentl
 ## Read Module Manifest
 $ModuleManifest = Import-PowerShellDataFile $ModuleManifestFileInfo.FullName -ErrorAction Stop
 
+## Output moduleName Azure Pipelines
 Write-Host ('##vso[task.setvariable variable=moduleName;isOutput=true]{0}' -f $ModuleManifestFileInfo.BaseName)
 Write-Host ('##[debug] {0} = {1}' -f 'moduleName', $ModuleManifestFileInfo.BaseName)
 
-Write-Host ('##vso[task.setvariable variable=moduleVersion;isOutput=true]{0}' -f $ModuleManifest.ModuleVersion)
-Write-Host ('##[debug] {0} = {1}' -f 'moduleVersion', $ModuleManifest.ModuleVersion)
+## Output moduleVersion Azure Pipelines
+[version] $ModuleVersion = $ModuleManifest.ModuleVersion
+if ($TrimRevisionNumber) { $ModuleVersion = $ModuleManifest.ModuleVersion -replace '(?<=^(.?[0-9]+){3,}).[0-9]+$', '' }
+Write-Host ('##vso[task.setvariable variable=moduleVersion;isOutput=true]{0}' -f $ModuleVersion)
+Write-Host ('##[debug] {0} = {1}' -f 'moduleVersion', $ModuleVersion)
 
 ## Read Packages Configuration
 if ($PackagesConfigFileInfo.Exists) {
@@ -29,6 +36,7 @@ if ($PackagesConfigFileInfo.Exists) {
     $xmlPackagesConfig.Load($PackagesConfigFileInfo.FullName)
 
     foreach ($package in $xmlPackagesConfig.packages.package) {
+        ## Output packageVersion Azure Pipelines
         Write-Host ('##vso[task.setvariable variable=version.{0};isOutput=true]{1}' -f $package.id, $package.version)
         Write-Host ('##[debug] version.{0} = {1}' -f $package.id, $package.version)
     }
